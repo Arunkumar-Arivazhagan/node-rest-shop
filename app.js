96f9eccec2ce072ae82e68066d1c7fada5c15f1a
arunkumar-codes <arunkumar041093@gmail.com>
2024-06-11 20:20:38 +1200

    sign-up flow with auth token

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
+const userRoutes = require("./api/routes/user");
 
 mongoose.connect(
   "mongodb+srv://node-shop:" +
@@ -15,8 +16,10 @@ mongoose.connect(
     userMongoClient: true,
   }
 );
+mongoose.Promise = global.Promise;
 
 app.use(morgan("dev"));
+app.use("/uploads", express.static("uploads"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json);
 
@@ -35,6 +38,7 @@ app.use((res, req, next) => {
 
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
+app.use("/user", userRoutes);
 
 app.use((res, req, next) => {
   const error = new Error("Not found");
